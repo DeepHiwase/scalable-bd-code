a1a3eb820f98870feb15cf8e0bc7fb64d1fab1e6
Deep <diphiwase0987@gmail.com>
2025-04-12 00:03:53 +0530

    Pnpm -install deps -set Hosted MongoURI

--- database/index.js
+++ database/index.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose"
-import { db } from "../config.js"
+// import { db } from "../config.js"
+import { dbURI } from "../config.js"
 
-const dbURI = `mongodb://${db.user}:${db.password}@${db.host}:${db.port}/${db.name}`
+// const dbURI = `mongodb://${db.user}:${db.password}@${db.host}:${db.port}/${db.name}`
 
 mongoose
   .connect(dbURI)
