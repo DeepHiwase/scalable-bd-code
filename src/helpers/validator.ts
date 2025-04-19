bea450f5b4b0a598cdc25563845588ec650014f0
Deep <diphiwase0987@gmail.com>
2025-04-19 22:17:15 +0530

    -install zod -Add userLogin Validation

---
+++ src/helpers/validator.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from "express";
+import { ZodError, ZodSchema } from "zod";
+import { BadRequestError } from "../core/CustomError";
+
+export enum ValidationSource {
+  BODY= "body",
+  QUERY= "query",
+  HEADER= "header",
+  PARAMS= "params"
+}
+
+const validateRequest = (schema: ZodSchema, source:ValidationSource = ValidationSource.BODY) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = schema.parse(req[source])
+      Object.assign(req[source], data)
+      next()
+    } catch (err) {
+      if (err instanceof ZodError) {
+        const message = err.errors.map(e => e.message).join(", ")
+        return next(new BadRequestError(message))
+      }
+      next(err)
+    }
+  }
+}
+
+export default validateRequest
